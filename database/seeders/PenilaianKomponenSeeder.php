189165cb00d00a7f2951f5f149e30a5d404d9bc0
rifqynrwndi <nirwandyrifqy@students.unnes.ac.id>
2026-07-14 20:21:00 +0800

    feat: Update PenilaianKomponenSeeder to adjust component weights and add missing component

--- database/seeders/PenilaianKomponenSeeder.php
+++ database/seeders/PenilaianKomponenSeeder.php
@@ -8,9 +8,10 @@ class PenilaianKomponenSeeder extends Seeder
     public function run(): void
     {
         $components = [
-            ['nama_komponen'=>'Logbook', 'deskripsi'=>'Nilai dari kualitas dan kelengkapan Logbook harian KKN', 'kategori'=>'dpl', 'bobot'=>20, 'urutan'=>1],
-            ['nama_komponen'=>'Pembekalan KKN UBT', 'deskripsi'=>'Nilai dari partisipasi dan evaluasi pembekalan KKN UBT', 'kategori'=>'lppm', 'bobot'=>20, 'urutan'=>3],
-            ['nama_komponen'=>'Seminar Hasil', 'deskripsi'=>'Nilai dari evaluasi laporan dan luaran oleh LPPM', 'kategori'=>'lppm', 'bobot'=>20, 'urutan'=>4],
+            ['nama_komponen'=>'Logbook', 'deskripsi'=>'Nilai dari kualitas dan kelengkapan Logbook harian KKN', 'kategori'=>'dpl', 'bobot'=>50, 'urutan'=>1],
+            ['nama_komponen'=>'Nilai Pelaksanaan KKN UBT', 'deskripsi'=>'Nilai Pelaksanaan KKN UBT', 'kategori'=>'dpl', 'bobot'=>30, 'urutan'=>2],
+            ['nama_komponen'=>'Pembekalan KKN UBT', 'deskripsi'=>'Nilai dari partisipasi dan evaluasi pembekalan KKN UBT', 'kategori'=>'lppm', 'bobot'=>10, 'urutan'=>3],
+            ['nama_komponen'=>'Seminar Hasil', 'deskripsi'=>'Nilai dari evaluasi laporan dan luaran oleh LPPM', 'kategori'=>'lppm', 'bobot'=>10, 'urutan'=>4],
         ];
 
         foreach ($components as $c) {
